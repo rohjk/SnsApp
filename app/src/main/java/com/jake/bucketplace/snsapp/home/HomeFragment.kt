d1c5a0ef3e091c715d4cd8d9218fa31683c001f5
JakeRoh <jake.roh@buzzvil.com>
2021-01-29 23:47:54 +0900

    set home fragment layout

--- app/src/main/java/com/jake/bucketplace/snsapp/home/HomeFragment.kt
+++ app/src/main/java/com/jake/bucketplace/snsapp/home/HomeFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.jake.bucketplace.snsapp.R
 import com.jake.bucketplace.snsapp.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
 
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var swipeContainer: SwipeRefreshLayout
+
+    private val viewModel: HomeViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,7 +23,20 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         this.binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.apply {
+            this@HomeFragment.swipeContainer = homeSwipeContainer
+        }
+
+
         return binding.root
     }
 
+    override fun onDestroy() {
+        binding.unbind()
+        super.onDestroy()
+    }
+
+    override fun onRefresh() {
+
+    }
 }
---
+++ app/src/main/java/com/jake/bucketplace/snsapp/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.jake.bucketplace.snsapp.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel: ViewModel() {
+
+}
