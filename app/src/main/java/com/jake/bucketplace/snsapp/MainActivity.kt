3047ef870575e0d81471dab8d2a0ff5b6e16dd99
JakeRoh <jake.roh@buzzvil.com>
2021-01-31 01:10:08 +0900

    add custom toolbar

--- app/src/main/java/com/jake/bucketplace/snsapp/MainActivity.kt
+++ app/src/main/java/com/jake/bucketplace/snsapp/MainActivity.kt
@@ -11,14 +11,28 @@ import com.jake.bucketplace.snsapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        this.binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         this.navController = navHostFragment.navController
 
+        binding.apply {
+            lifecycleOwner = this@MainActivity
+            this@MainActivity.toolbar = mainToolbar
+        }
+
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController)
+        val actionBar = supportActionBar
+        actionBar?.apply {
+            setDisplayShowCustomEnabled(true)
+            setDisplayShowTitleEnabled(false)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
