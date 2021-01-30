6376a44d9a845571e8e6a2f9f4480b562ed6049a
JakeRoh <jake.roh@buzzvil.com>
2021-01-31 01:19:25 +0900

    set nav

--- app/src/main/java/com/jake/bucketplace/snsapp/MainActivity.kt
+++ app/src/main/java/com/jake/bucketplace/snsapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.jake.bucketplace.snsapp.databinding.ActivityMainBinding
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.apply {
             lifecycleOwner = this@MainActivity
+            activity = this@MainActivity
             this@MainActivity.toolbar = mainToolbar
         }
 
@@ -37,5 +39,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    fun signUp() {
+        this.navController.navigate(NavHomeDirections.actionGlobalSignUpFragment())
+    }
+
+    fun signIn() {
+
+    }
+
+    fun singOut() {
+
     }
 }
---
+++ app/src/main/java/com/jake/bucketplace/snsapp/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.jake.bucketplace.snsapp
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel constructor(
+
+): ViewModel() {
+
+}
