91d99e53bbe6446c4ff369cb1bcf5cfe0403da14
JakeRoh <jake.roh@buzzvil.com>
2021-01-31 15:50:57 +0900

    create card mapper test

---
+++ app/src/test/java/com/jake/bucketplace/snsapp/data/model/mapper/CardMapperTest.kt
@@ -0,0 +1,43 @@
+package com.jake.bucketplace.snsapp.data.model.mapper
+
+import com.jake.bucketplace.snsapp.data.model.Card
+import io.mockk.MockKAnnotations
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class CardMapperTest {
+
+    private val cardMapper = CardMapper()
+
+    private val testCardId: Long = 1
+    private val testUserId: Long = 2
+    private val testDescription: String = "TEST_DESC"
+    private val testImageUrl: String = "TEST_IMAGE_URL"
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun testTransform_success() {
+        val inputDataCard = Card(
+            id = testCardId,
+            userId = testUserId,
+            description = testDescription,
+            imageUrl = testImageUrl
+        )
+
+        val expectedDomainCard = com.jake.bucketplace.snsapp.domain.model.Card (
+            id = testCardId,
+            userId = testUserId,
+            description = testDescription,
+            imageUrl = testImageUrl
+        )
+
+        val actual = cardMapper.transform(inputDataCard)
+
+        Assert.assertEquals(expectedDomainCard, actual)
+    }
+}
